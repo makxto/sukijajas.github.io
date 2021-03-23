e1d1d419612125ff39d090b27858bfdd7a198725
makxto <laudy.neymar@gmail.com>
2021-03-23 07:40:25 +0700

    semoga sukses!

---
+++ js/script.js
@@ -0,0 +1,57 @@
+var d,h,m,s,animate;
+
+function init(){
+    d=new Date();
+    h=d.getHours();
+    m=d.getMinutes();
+    s=d.getSeconds();
+    clock();
+};
+
+function clock(){
+    s++;
+    if(s==60){
+        s=0;
+        m++;
+        if(m==60){
+            m=0;
+            h++;
+            if(h==24){
+                h=0;
+            }
+        }
+    }
+    $('sec',s);
+    $('min',m);
+    $('hr',h);
+    animate=setTimeout(clock,1000);
+};
+
+
+document.write("<center><font size=20em, style='color: white;'  'font-family: Helvetica, sans-serif;'>");
+var day = new Date();
+var hr = day.getHours();
+if (hr >= 6 && hr < 12) {
+    document.write("Good Morning");
+} else if (hr == 12) {
+    document.write("Good Noon");
+} else if (hr >= 12 && hr <= 15) {
+    document.write("Good Afternoon");
+} else if (hr >= 21 && hr <= 0) {
+    document.write("Good Night");
+} else {
+    document.write("Good Evening");
+}
+document.write("</font></center>");
+
+function $(id,val){
+    if(val<10){
+        val='0'+val;
+    }
+    document.getElementById(id).innerHTML=val;
+};
+
+window.onload=init;
+
+
+
